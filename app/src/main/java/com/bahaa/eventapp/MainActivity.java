090117b5e4231623c50f36802e74d142bd7561d6
Bahaaib <bahaasco@gmail.com>
2019-07-01 00:03:28 +0200

    added 0pdd

--- app/src/main/java/com/bahaa/eventapp/MainActivity.java
+++ app/src/main/java/com/bahaa/eventapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // @todo #1 test 0pddd configs
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
