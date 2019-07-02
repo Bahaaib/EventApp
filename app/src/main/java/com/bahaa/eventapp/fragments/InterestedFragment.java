a5f27d4e7454b8a60387d6683d850c8fd6ddf02d
Bahaaib <bahaasco@gmail.com>
2019-07-02 15:32:12 +0200

    init empty home fragments

---
+++ app/src/main/java/com/bahaa/eventapp/fragments/InterestedFragment.java
@@ -0,0 +1,32 @@
+package com.bahaa.eventapp.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bahaa.eventapp.R;
+
+
+public class InterestedFragment extends Fragment {
+
+
+    public InterestedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_interested, container, false);
+
+        return v;
+    }
+
+}
