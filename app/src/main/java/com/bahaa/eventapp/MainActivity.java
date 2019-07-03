ee86fdb178922d27adb16977116e6222ed8bb17d
Bahaaib <bahaasco@gmail.com>
2019-07-03 18:41:57 +0200

    added drawer toggle func

--- app/src/main/java/com/bahaa/eventapp/MainActivity.java
+++ app/src/main/java/com/bahaa/eventapp/MainActivity.java
@@ -1,12 +1,18 @@
 package com.bahaa.eventapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,12 +20,20 @@ import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.main_toolbar)
+    public Toolbar toolbar;
     @BindView(R.id.btm_nav)
     public BottomNavigationView bottomNavigationView;
     @BindView(R.id.pager)
     public ViewPager viewPager;
+    @BindView(R.id.drawer_layout)
+    public DrawerLayout drawerLayout;
+    @BindView(R.id.nv)
+    public NavigationView navigationView;
 
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     private Unbinder unbinder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,15 @@ public class MainActivity extends AppCompatActivity {
 
         unbinder = ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+
+        setupViewPager();
+        setupBottomNavigationView();
+        setupNavigationDrawer();
+
+    }
+
+    private void setupViewPager() {
         final PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), 3);
         viewPager.setAdapter(pagerAdapter);
 
@@ -49,7 +72,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    private void setupBottomNavigationView() {
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.action_nearby:
@@ -64,5 +89,55 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
+    }
+
+    private void setupNavigationDrawer() {
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.colorAccent));
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.action_nearby:
+                    displayToast("Clicked Nearby");
+                    return true;
+
+                case R.id.action_explore:
+                    displayToast("Clicked Explore");
+                    return true;
+
+                case R.id.action_interested:
+                    displayToast("Clicked Interested");
+                    return true;
+
+                default:
+                    return true;
+            }
+        });
+    }
+
+    private void displayToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
