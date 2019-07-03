adf5a8e2d56d27463f5739b9a1b30458c93b2854
Bahaaib <bahaasco@gmail.com>
2019-07-04 01:28:20 +0200

    added search bar + designed horizotal card

--- app/src/main/java/com/bahaa/eventapp/fragments/ExploreFragment.java
+++ app/src/main/java/com/bahaa/eventapp/fragments/ExploreFragment.java
@@ -3,16 +3,28 @@ package com.bahaa.eventapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.bahaa.eventapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 
 public class ExploreFragment extends Fragment {
+
+    @BindView(R.id.search_view)
+    public SearchView searchView;
+
+    private Unbinder unbinder;
 
 
     public ExploreFragment() {
@@ -26,7 +38,18 @@ public class ExploreFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_explore, container, false);
 
+        unbinder = ButterKnife.bind(this, v);
+
+
+
+
         return v;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        unbinder.unbind();
+    }
 }
