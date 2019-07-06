bc06e1341601046c9539901063e1057a5df663ee
Bahaaib <bahaasco@gmail.com>
2019-07-06 04:21:17 +0200

    added new puzzles

--- app/src/main/java/com/bahaa/eventapp/fragments/ExploreFragment.java
+++ app/src/main/java/com/bahaa/eventapp/fragments/ExploreFragment.java
@@ -42,7 +42,7 @@ public class ExploreFragment extends Fragment {
     private ArrayList<EventModel> futureEventsList;
     private FutureEventsAdapter futureEventsAdapter;
 
-
+    // @todo #3 added interested button to future events
     public ExploreFragment() {
         // Required empty public constructor
     }
