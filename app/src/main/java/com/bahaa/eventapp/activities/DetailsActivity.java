33daf5ded0240ceb467fb99fed3b55c98e29ff23
Bahaaib <bahaasco@gmail.com>
2019-07-22 23:27:57 +0200

    init Details Activity

---
+++ app/src/main/java/com/bahaa/eventapp/activities/DetailsActivity.java
@@ -0,0 +1,16 @@
+package com.bahaa.eventapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bahaa.eventapp.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
