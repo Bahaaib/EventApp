cf613af5ee7b2a325372a7e30b8ae72f07b62f05
Bahaaib <bahaasco@gmail.com>
2019-07-23 10:09:13 +0200

    added expandable description

--- app/src/main/java/com/bahaa/eventapp/activities/DetailsActivity.java
+++ app/src/main/java/com/bahaa/eventapp/activities/DetailsActivity.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.OvershootInterpolator;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bahaa.eventapp.R;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
+import at.blogc.android.views.ExpandableTextView;
+import butterknife.BindDrawable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 public class DetailsActivity extends AppCompatActivity {
@@ -24,6 +31,16 @@ public class DetailsActivity extends AppCompatActivity {
 
     @BindView(R.id.details_event_toolbar)
     public Toolbar toolbar;
+
+    @BindView(R.id.details_event_address)
+    public TextView address;
+
+    @BindDrawable(R.drawable.ic_location)
+    public Drawable locationIcon;
+
+    @BindView(R.id.details_event_description)
+    public ExpandableTextView description;
+
 
     private Unbinder unbinder;
 
@@ -36,14 +53,18 @@ public class DetailsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setupActionBar();
         setupCollapsingToolbar();
+        resizeAddressDrawable();
+        setupExpandableDescription();
+
+
 
 
     }
 
-    private void setupActionBar(){
+    private void setupActionBar() {
         ActionBar actionBar = getSupportActionBar();
 
-        if (actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
@@ -55,6 +76,43 @@ public class DetailsActivity extends AppCompatActivity {
         collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedText);
     }
 
+    private void resizeAddressDrawable() {
+        locationIcon.setBounds(
+                0,
+                0,
+                convertToPixels(24),
+                convertToPixels(24));
+        address.setCompoundDrawables(locationIcon, null, null, null);
+
+    }
+
+    private int convertToPixels(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+
+    }
+
+    private void setupExpandableDescription(){
+
+        description.setInterpolator(new OvershootInterpolator());
+
+        String descriptionText = "It is a long established fact that a reader will be distracted by " +
+                "the readable content of a page when looking at its layout. The point of using Lorem" +
+                " Ipsum is that it has a more-or-less normal distribution of letters, as opposed to" +
+                " using 'Content here, content here', making it look like readable English. Many " +
+                "desktop publishing packages and web page editors now use Lorem Ipsum as their" +
+                " default model text, and a search for 'lorem ipsum' will uncover many web sites" +
+                " still in their infancy. Various versions have evolved over the years, sometimes" +
+                " by accident, sometimes on purpose (injected humour and the like).";
+        description.setText(descriptionText);
+    }
+
+    @OnClick(R.id.details_event_description)
+    public void toggleExpandableDescription(){
+
+        description.toggle();
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.details_menu, menu);
@@ -64,13 +122,13 @@ public class DetailsActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        switch (id){
+        switch (id) {
             case R.id.action_order:
-                Toast.makeText(getApplicationContext(),"Booked!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Booked!", Toast.LENGTH_LONG).show();
                 return true;
 
             default:
-                 return super.onOptionsItemSelected(item);
+                return super.onOptionsItemSelected(item);
         }
 
     }
