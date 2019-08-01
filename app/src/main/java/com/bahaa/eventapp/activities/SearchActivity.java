a1b185856da793b5fb9fa36de2ac68919325aef4
Bahaaib <bahaasco@gmail.com>
2019-08-01 23:06:40 +0200

    added Search Activity

---
+++ app/src/main/java/com/bahaa/eventapp/activities/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.bahaa.eventapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bahaa.eventapp.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
